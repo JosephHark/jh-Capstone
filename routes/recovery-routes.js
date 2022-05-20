6a5acdbe6834c0c31942daddbadb4b46e0155c40
JosephHark <har17033@byui.edu>
2022-05-20 13:09:48 -0400

    5-20

--- routes/recovery-routes.js
+++ routes/recovery-routes.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
 const info = (req, res, next) => {
     if (req.user) {
-        //if user is not logined in
-        next();
-    } else {
-        //if logged in
+        //if user is  logined in
         next();
     }
 };
@@ -12,12 +9,12 @@ router.get('/technology', (req, res) => {
     res.render('technology'), info, {user:req.user};
 });
 router.get('/gambling', (req, res) => {
-    res.render('gambling'), {user:req.user};
+    res.render('gambling'),info, {user:req.user};
 });
 router.get('/sexual', (req, res) => {
-    res.render('sexual'), {user:req.user};
+    res.render('sexual'),info, {user:req.user};
 });
 router.get('/substance', (req, res) => {
-    res.render('substance'), {user:req.user};
+    res.render('substance'),info, {user:req.user};
 });
 module.exports = router;
