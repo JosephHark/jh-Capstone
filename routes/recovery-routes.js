a21ddeeedf033fc5578bf4797e70661a3202d2d1
JosephHark <har17033@byui.edu>
2022-05-28 15:04:25 -0400

    css

--- routes/recovery-routes.js
+++ routes/recovery-routes.js
@@ -5,12 +5,12 @@ router.get('/technology', (req, res) => {
     res.render('technology', {user:req.user});
 });
 router.get('/gambling', (req, res) => {
-    res.render('gambling',info, {user:req.user});
+    res.render('gambling',{user:req.user});
 });
 router.get('/sexual', (req, res) => {
-    res.render('sexual',info, {user:req.user});
+    res.render('sexual', {user:req.user});
 });
 router.get('/substance', (req, res) => {
-    res.render('substance',info, {user:req.user});
+    res.render('substance', {user:req.user});
 });
 module.exports = router;
