cf336554e896bdc7a3345e931085612c5c659585
JosephHark <har17033@byui.edu>
2022-06-05 01:31:50 -0400

    6-5

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { render } = require('express/lib/response');
+const Contacts = require('../models/contacts-model');
 const contacts = require('../models/contacts-model');
 const authCheck = (req, res, next) => {
     if (!req.user) {
@@ -9,15 +11,40 @@ const authCheck = (req, res, next) => {
         next();
     }
 };
-
+app.use(express.urlencoded({extended : true}));
 router.get('/', authCheck, (req, res) => {
     contacts.find()
         .then((result) => {
+            console.log(contacts)
             res.render('profile', {user: req.user}, {contacts: result});
         })
         .catch((err) => {
             console.log(err)
         })
 });
+app.post('/', authCheck, (req, res) => {
+    const contact = new Contact(req.body)
+    contact.save()
+    .then =((result)=>{
+        res.redirect('profile', {user: req.user}, {contacts: result});
+    });
+});
 
+app.get('/', authCheck, (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    Contacts.findById(id)
+    .then(result=>{
+        render('profile', {user: req.user});
+    })
+})
+
+app.get('/', authCheck, (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    Contacts.findById(id)
+    .then(result=>{
+        render('profile', {user: req.user});
+    })
+})
 module.exports = router;
